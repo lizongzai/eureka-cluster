5e6effcb1f7f282b759b2f76e368f34197ef3c68
root <lizongzai@gmail.com>
2023-02-24 10:13:03 +0800

    V4 增加Swagger2接口文档管理工具

---
+++ service-provider/src/main/java/com/example/serviceprovider/config/swagger/Swagger2Config.java
@@ -0,0 +1,37 @@
+package com.example.serviceprovider.config.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+  public Docket createRestApi() {
+
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.example.serviceprovider.controller"))
+        .paths(PathSelectors.any())
+        .build();
+
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("Restful API Document")
+        .description("Restful API Document")
+        .contact(new Contact("lizongzai", "http://localhost:8080/doc.html", "lizongzai@gmail.com"))
+        .version("1.0")
+        .build();
+  }
+
+}
